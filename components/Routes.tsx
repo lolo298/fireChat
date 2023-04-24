3d94d7e45fd38cd5b9c2d3acc7b258a5305f1b67
lolo298 <lorenzoaoulini@gmail.com>
2023-04-24 22:24:56 +0200

    realtime db setup

--- components/Routes.tsx
+++ components/Routes.tsx
@@ -1,12 +1,22 @@
 import { AuthContext } from "@utils/context";
 import { useContext } from "react";
 
-export function UserRoute({ children }) {
+export function UserRoute({ children, needAdmin = false, needLogged = false }: UserRouteProps) {
   const userContext = useContext(AuthContext);
-  console.log(userContext);
-  if (!userContext) {
-    return <h1>Not logged in</h1>;
+
+  if (needLogged && !userContext) {
+    return <div>Not logged in</div>;
   }
 
+  // if (needAdmin && !userContext?.isAdmin) {
+  //   return <div>Not an admin</div>;
+  // }
+
   return <>{children}</>;
+}
+
+interface UserRouteProps {
+  children: React.ReactNode;
+  needAdmin?: boolean;
+  needLogged?: boolean;
 }
