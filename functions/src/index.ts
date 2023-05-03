37638ff5b7c4afe8e53fab2474ca5e082cb634e3
lolo298 <lorenzoaoulini@gmail.com>
2023-05-03 15:11:50 +0200

    Repair cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,13 @@
-import functions from "firebase-functions";
-import { auth, initializeApp, credential } from "firebase-admin";
-import * as dotenv from "dotenv";
-dotenv.config();
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
-initializeApp({
-  credential: credential.cert({
-    projectId: process.env.PROJECT_ID,
-    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    clientEmail: process.env.CLIENT_EMAIL,
-  }),
-  databaseURL: "https://firechat-aa8ae-default-rtdb.europe-west1.firebasedatabase.app",
-});
-
-export const helloWorld = functions.auth.user().onCreate((user) => {
-  auth().setCustomUserClaims(user.uid, { admin: true });
-  console.log("user", user);
-  return { data: "Hello from Firebase!" };
+export const SetupUser = functions.auth.user().onCreate(async (user) => {
+  try {
+    // throw new Error("This is a test error");
+    await admin.auth().setCustomUserClaims(user.uid, { role: "user" });
+    return { data: "success" };
+  } catch (error) {
+    return { data: error };
+  }
 });
